26010b3661623961c41b1dc736db0e3a60ed120b
marvin <sea-son@foxmail.com>
2025-01-16 18:44:01 +0800

    code: code

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,21 @@
 import { AdvancedRichEditorProvider } from '@/components/advanced-rich-editor'
 import Operator from '@/components/rich-editor/operator/operator'
 import ModelSetting from '@/components/setting'
-import { Separator } from '@/components/ui/separator'
+const content = `
+\`\`\`js
+const a = () => {
+}
+\`\`\`
+`
 
 export const Index = () => {
   return (
     <div className="p-8 pt-2 relative ">
       <AdvancedRichEditorProvider
+        content={content}
         slotBefore={
           <div className="sticky top-2 flex justify-end items-center border-b mb-4">
-            <ModelSetting/>
+            <ModelSetting />
             <Operator />
           </div>
         }
