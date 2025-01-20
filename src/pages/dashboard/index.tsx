6f0009b1c026152bfda0ba08d0133c1ab4b79eb0
marvin <sea-son@foxmail.com>
2025-01-20 14:14:30 +0800

    调整代码

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,10 +2,10 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <>
+    <div className={'flex flex-col h-[100dvh]'}>
       <nav
         className={
-          "navbar bg-white shadow-lg backdrop-blur px-8 py-6 flex gap-4 font-bold text-lg border border-muted"
+          "navbar sticky top-0 bg-white shadow-lg backdrop-blur px-8 py-6 flex gap-4 font-bold text-lg border border-muted"
         }
       >
         <NavLink to={"/"} className="navbar-brand">
@@ -15,7 +15,9 @@ export default function Dashboard() {
           日志
         </NavLink>
       </nav>
-      <Outlet />
-    </>
+      <div className={'flex-1'}>
+        <Outlet />
+      </div>
+    </div>
   );
 }
