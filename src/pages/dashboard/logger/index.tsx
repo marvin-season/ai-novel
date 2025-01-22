fe1af306789a8fb41a80977847dd43cb183891e3
marvin <sea-son@foxmail.com>
2025-01-22 15:09:08 +0800

    fix: 删除 zustand

--- src/pages/dashboard/logger/index.tsx
+++ src/pages/dashboard/logger/index.tsx
@@ -1,14 +1,5 @@
-import useStore from "@/store";
-import { useEffect } from "react";
-
 export default function Logger() {
-  const { count, increment, loadFromIDB } = useStore();
-
-
-  // 加载初始状态
-  useEffect(() => {
-    loadFromIDB();
-  }, [loadFromIDB]);
+  const { count, increment, loadFromIDB } = {}
   return (
     <div>
       {count}
