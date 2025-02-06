b4395c48d05bbdb826f1db6dca796f6dede8620b
marvin <sea-son@foxmail.com>
2025-02-06 17:54:33 +0800

    fix: 使用 shadcn的toast替换sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Toaster } from "sonner";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
 import { Provider } from "react-redux";
 import store from "@/store";
+import { useEffect } from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
 console.log("app")
 function App() {
+  const { toast } = useToast();
+  useEffect(() => {
+    toast({
+      title: "Scheduled: Catch up",
+      description: "Friday, February 10, 2023 at 5:57 PM",
+    })
+  }, [])
   return (
     <>
       <Toaster />
