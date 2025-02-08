928cae323877a803bb5ee536681446b1709cc405
marvin <sea-son@foxmail.com>
2025-02-08 15:24:53 +0800

    feat: base case for block suite

--- src/pages/dashboard/canvas/index.tsx
+++ src/pages/dashboard/canvas/index.tsx
@@ -1,5 +1,12 @@
-export default function Canvas(){
-    return <>
-    
-    </>
+import EditorContainer from "@/components/page-editor/editor-container";
+import { EditorProvider } from "./provider";
+
+export default function Canvas() {
+    return <EditorProvider>
+        <div className="app">
+            <div className="main-content">
+                <EditorContainer />
+            </div>
+        </div>
+    </EditorProvider>
 }
