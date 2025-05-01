467feb3ca643be5b6c69655c6f1c9917503e6db4
marvin <sea-son@foxmail.com>
2025-05-01 21:44:43 +0800

    👌 IMPROVE: 删除不用的依赖

--- src/pages/dashboard/canvas/editor/editor.ts
+++
@@ -1,9 +0,0 @@
-import { createEmptyDoc, PageEditor } from '@blocksuite/presets';
-import '@blocksuite/presets/themes/affine.css';
-
-export function initEditor() {
-  const doc = createEmptyDoc().init();
-  const editor = new PageEditor();
-  editor.doc = doc;
-  return { editor };
-}
