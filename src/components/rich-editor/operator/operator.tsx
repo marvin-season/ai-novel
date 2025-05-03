6ecaf432e5eec921db052b190bd5e1cd295c46e8
marvin <sea-son@foxmail.com>
2025-05-03 20:24:52 +0800

    👌 IMPROVE: code pretty

--- src/components/rich-editor/operator/operator.tsx
+++ src/components/rich-editor/operator/operator.tsx
@@ -1,49 +1,18 @@
 import {
   Code2,
-  FileInputIcon,
+  Download,
   Redo2,
-  SaveAll,
-  SaveIcon,
   Undo2,
+  Upload,
 } from "lucide-react";
 import { Editor, useCurrentEditor } from "@tiptap/react";
 import { toast } from "sonner";
 import { Separator } from "@/components/ui/separator";
 import { IconSizeMiddle as size } from "@/constants";
 import { useDebounce } from "ahooks";
+import { handleExport, handleImport } from "@/utils";
 
-function handleExport(editor: Editor) {
-  // const json = editor.getJSON();
-  const md = editor.storage.markdown.getMarkdown();
-  // const blob = new Blob([JSON.stringify(json)], { type: 'application/json' });
-  const blob = new Blob([md], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "editor-content.md";
-  a.click();
-  URL.revokeObjectURL(url);
-  toast.success("Exported successfully!");
-}
 
-async function handleImport(
-  editor: Editor,
-  event: React.ChangeEvent<HTMLInputElement>,
-) {
-  const file = event.target.files?.[0];
-  if (file) {
-    if (!file.name.endsWith(".md")) {
-      toast.error("Invalid file format.");
-      return;
-    }
-    const text = await file.text();
-    // const json = JSON.parse(text);
-    editor.commands.setContent(text);
-    toast.success("Imported successfully!");
-  } else {
-    toast.error("No file selected.");
-  }
-}
 
 export default function Operator() {
   const { editor } = useCurrentEditor();
@@ -59,11 +28,33 @@ export default function Operator() {
   return (
     <>
       <div
+        className="cursor-pointer rounded-sm bg-slate-50 "
+        onClick={() => handleExport(editor)}
+      >
+        <Download size={size} />
+      </div>
+
+      <input
+        type="file"
+        accept=".md,.txt"
+        style={{ display: "none" }}
+        id="import-md"
+        onChange={(event) => handleImport(editor, event)}
+      />
+      <label
+        htmlFor="import-md"
+        className="cursor-pointer rounded-sm bg-slate-50 "
+      >
+        <Upload size={size} />
+      </label>
+      <Separator className="w-full" orientation="horizontal" />
+
+      {/* <div
         className="cursor-pointer rounded-sm bg-slate-50  "
         onClick={handleUpdate}
       >
         <SaveAll size={size} />
-      </div>
+      </div> */}
       <div
         className="cursor-pointer rounded-sm bg-slate-50  "
         onClick={() => editor.chain().focus().undo().run()}
@@ -82,27 +73,7 @@ export default function Operator() {
       >
         <Code2 size={size} />
       </div>
-      <Separator className="w-full" orientation="horizontal" />
-      <div
-        className="cursor-pointer rounded-sm bg-slate-50 "
-        onClick={() => handleExport(editor)}
-      >
-        <SaveIcon size={size} />
-      </div>
 
-      <input
-        type="file"
-        accept=".md,.txt"
-        style={{ display: "none" }}
-        id="import-md"
-        onChange={(event) => handleImport(editor, event)}
-      />
-      <label
-        htmlFor="import-md"
-        className="cursor-pointer rounded-sm bg-slate-50 "
-      >
-        <FileInputIcon size={size} />
-      </label>
     </>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { Editor } from "@tiptap/core";
+import { toast } from "sonner";
 
 
 // 下载 TXT 文件
@@ -9,3 +11,36 @@ export const handleDownloadText = (text: string) => {
   link.download = 'converted.txt';
   link.click();
 };
+
+export function handleExport(editor: Editor) {
+  // const json = editor.getJSON();
+  const md = editor.storage.markdown.getMarkdown();
+  // const blob = new Blob([JSON.stringify(json)], { type: 'application/json' });
+  const blob = new Blob([md], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "editor-content.md";
+  a.click();
+  URL.revokeObjectURL(url);
+  toast.success("Exported successfully!");
+}
+
+export async function handleImport(
+  editor: Editor,
+  event: React.ChangeEvent<HTMLInputElement>,
+) {
+  const file = event.target.files?.[0];
+  if (file) {
+    if (!file.name.endsWith(".md")) {
+      toast.error("Invalid file format.");
+      return;
+    }
+    const text = await file.text();
+    // const json = JSON.parse(text);
+    editor.commands.setContent(text);
+    toast.success("Imported successfully!");
+  } else {
+    toast.error("No file selected.");
+  }
+}
