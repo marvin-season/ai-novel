3e8c40d10ef1364f741e2098d89d6f1940b1a91e
marvin <sea-son@foxmail.com>
2025-05-04 14:44:25 +0800

    👌 IMPROVE: chat to editor

--- src/components/messages/BotMessage.tsx
+++ src/components/messages/BotMessage.tsx
@@ -1,9 +1,10 @@
 import React, { memo } from "react";
 import { Bubble } from '@ant-design/x';
 import { BaseMessageProps } from "@/types";
-import { BotIcon } from "lucide-react";
+import { BotIcon, CornerDownLeft } from "lucide-react";
 import { IconSizeSmall } from "@/constants";
 import { IMessageStatus } from "@/store/agentStore";
+import { useCurrentEditor } from "@tiptap/react";
 /**
  * Props for the UserMessage component
  */
@@ -14,9 +15,13 @@ interface BotMessageProps extends BaseMessageProps {
  * User message UI component
  */
 export const BotMessage = memo(({ message }: BotMessageProps) => {
+  const { editor } = useCurrentEditor();
   return (
     <Bubble
       header={<span className="text-xs text-gray-400">{new Date(message.timestamp!).toLocaleString()}</span>}
+      footer={<CornerDownLeft className="text-blue-500" size={IconSizeSmall} onClick={() => {
+        editor?.commands.insertContent(message.content)
+      }} />}
       loading={message.status === IMessageStatus.loading}
       placement="start"
       content={message.content}
@@ -26,6 +31,5 @@ export const BotMessage = memo(({ message }: BotMessageProps) => {
           backgroundColor: '#d4f4f5',
         }
       }} />
-
   );
 });
